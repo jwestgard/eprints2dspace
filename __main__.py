ba3e4a0643d96fc038e40cfcab9b630980e44f7e
Joshua Westgard <jwestgard@mac.com>
2019-05-14 12:46:59 -0400

    add yaml config class and requirements.txt

--- __main__.py
+++ __main__.py
@@ -3,7 +3,21 @@
 import argparse
 from eprints import Eprint
 from dspace import SafPackage
-import pyyaml
+import sys
+import yaml
+
+
+class Config():
+
+    '''Project configuration class'''
+
+    def __init__(self, path):
+        with open(path) as configfile:
+            self._config = yaml.safe_load(configfile)
+    
+    def get_property(self, property_name):
+        '''Return the value of key in the yaml or None if not found''' 
+        return self._config.get(property_name, None)
 
 
 
@@ -14,3 +28,6 @@ if __name__ == "__main__":
 
     saf = SafPackage('foobar/')
     print(saf.packagedir)
+
+    config = Config('config.yml')
+    print(config.get_property('eprints_host'))
