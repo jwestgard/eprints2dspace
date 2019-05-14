4345f327bccb66c1b5197321d8b14296916d189a
Joshua Westgard <westgard@Joshuas-iMac.fios-router.home>
2019-05-14 18:52:49 -0400

    server class and build query to get DC metadata

--- __main__.py
+++ __main__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
-from eprints import Eprint
+from eprints import Eprint, Server
 from dspace import SafPackage
 import sys
 import yaml
@@ -20,28 +20,37 @@ class Config():
         return self._config.get(property_name, None)
 
 
+def fetch_data(id, config):
+    local_dir = config.get_property('export_dir')
+    print(local_dir)
 
 
-'''
-(1) Start from list of eprint ids
-(2) Pull metadata or read files from data dir
-(3) Parse metadata file 
-(4) Transform metadata
-(5) Write to SAF package
-'''
 
 
 
 
 if __name__ == "__main__":
-    
-    e = Eprint(10)
-    print(e, e.id)
-    e.parse_dc_file('data/export/minorityhealth-archive-3071.txt')
-    print(e.__dict__)
-
-    saf = SafPackage('foobar/')
-    print(saf.packagedir)
 
     config = Config('config.yml')
-    print(config.get_property('eprints_host'))
+
+
+    '''
+    (1) Start from list of eprint ids
+    (2) Pull metadata or read files from data dir
+    (3) Parse metadata file 
+    (4) Transform metadata
+    (5) Write to SAF package
+    '''
+    
+    e = Eprint(10)
+    # print(e, e.id)
+    e.parse_dc_file('data/export/minorityhealth-archive-3090.txt')
+    e.display_metadata()
+    
+
+    saf = SafPackage('foobar/')
+    # print(saf.packagedir)
+
+    data = fetch_data('3090', config)
+    server = Server(config)
+    server.get('3090')
--- eprints.py
+++ eprints.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+import os
+import requests
+
+
 class Eprint():
 
     '''Python representation of a single resource from an Eprints repository'''
@@ -7,9 +11,12 @@ class Eprint():
     def __init__(self, id):
         self.id = id
 
+
     def parse_dc_file(self, filepath):
+    
         '''Parse a textfile of dublin-core keys and values 
            into attributes on the python object'''
+           
         with open(filepath, 'r') as inputfile:
             contents = inputfile.read().split('\n')
             
@@ -21,13 +28,29 @@ class Eprint():
             else:
                 # split line on first colon
                 key, value = tuple(line.split(': ', 1))
-                # default to empty list so any attribute can be multivalued
-                default = list()
-                new = getattr(self, key, default)
-                setattr(self, key, new.append(value))
-                print('\n', n)
-                print(key)
-                print(new)
-                print(value)
-                print(getattr(self, key))
-               
+                # default to empty list
+                attrib_list = getattr(self, key, [])
+                # update the existing value list and then the attribute
+                attrib_list.append(value)
+                setattr(self, key, attrib_list)
+
+
+    def display_metadata(self):
+        for k, v in self.__dict__.items():
+            print("{} => {}".format(k, v))
+
+
+
+class Server():
+
+    '''Host Eprints server from which data is to be exported'''
+    
+    def __init__(self, config):
+        self.host_name = config.get_property('eprints_host')
+        self.archive_name = config.get_property('eprints_archive')
+        self.dc_path = config.get_property('dc_export_path')
+
+
+    def get(self, id):
+        query = os.path.join(self.host_name, self.dc_path.format(id, self.archive_name))
+        print(query)
