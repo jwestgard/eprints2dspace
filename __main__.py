b63dd6b70bbee26cb00aa3366118e2c8a1373d77
Joshua Westgard <jwestgard@mac.com>
2019-05-15 17:06:05 -0400

    trying for better metadata differentiation

--- __main__.py
+++ __main__.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
 import argparse
+import csv
 from eprints import Eprint
 from dspace import SafPackage
+import os
+import requests
 import sys
 import yaml
 
@@ -20,19 +23,74 @@ class Config():
         return self._config.get(property_name, None)
 
 
+class Batch():
+
+    '''Class for managing a particular migration batch'''
+
+    def __init__(self, config):
+        self.query_template = config.get_property('QUERY_TEMPLATE')
+        self.archive_name   = config.get_property('ARCHIVE_NAME')
+        self.export_range   = config.get_property('EXPORT_RANGE')
+        self.destination    = config.get_property('DESTINATION')
+        self.local_dir      = config.get_property('EPRINT_EXPORT')
+        self.mapfile        = config.get_property('MAPFILE')
+        self.ids            = self.get_id_range()
+        self.cursor         = 0
+
+    def get_id_range(self):
+        begin, end = self.export_range.split('-')
+        return [id for id in range(int(begin), int(end)+1)]
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.cursor < len(self.ids):
+            id = self.ids[self.cursor]
+            self.cursor += 1
+            return id
+        else:
+            #self.logger.info('Processing complete!')
+            raise StopIteration()
+
 
 def main():
 
-    config = Config('config.yml')
+    '''(1) Start from list of eprint ids'''
 
-    for id in range(3080, 3100):
-    
-        eprint = Eprint(id, config)
-        eprint.fetch_metadata()
-        eprint.display_metadata()
+    config = Config('config.yml')
+    batch = Batch(config)
+
+    with open('data/errors.csv', 'w') as errfile, open('data/active.csv', 'w') as actfile:
+        errors = csv.writer(errfile)
+        active = csv.writer(actfile)
         
-        saf = SafPackage(config)
-        saf.write(eprint)
+        '''(2) Pull metadata or read files from data dir'''
+        
+        for id in batch:
+            eprint = Eprint(id)
+            local_path = os.path.join(batch.local_dir, eprint.filename)
+            
+            # check for a cached metadata file, pull from server if not found
+            if not os.path.isfile(local_path):
+                query = batch.query_template.format(id, batch.archive_name)
+                response = requests.get(query)
+                if response.status_code == 200:
+                    with open(local_path, 'w') as handle:
+                        handle.write(response.text)
+                else:
+                    errors.writerow([id, response.status_code, query])
+                    continue
+                
+            '''(3) Parse metadata file'''
+            
+            with open(local_path, 'r') as handle:
+                eprint.parse(handle.read())
+                active.writerow([eprint.id, eprint.title, eprint.relation])
+
+
+    '''(4) Transform metadata'''
+    '''(5) Write to SAF package'''
 
 
 
@@ -40,10 +98,4 @@ if __name__ == "__main__":
     main()
         
 
-    '''
-    (1) Start from list of eprint ids
-    (2) Pull metadata or read files from data dir
-    (3) Parse metadata file 
-    (4) Transform metadata
-    (5) Write to SAF package
-    '''
+
--- eprints.py
+++ eprints.py
@@ -3,62 +3,47 @@
 import os
 import requests
 
-
 class Eprint():
 
     '''Python representation of a single resource from an Eprints repository'''
 
-    def __init__(self, id, config):
-        host = config.get_property('eprints_host')
-        query_path = config.get_property('dc_export_path')
-        archive = config.get_property('eprints_archive')
-        self.id = id
-        self.filename = f"{self.id}.txt"
-        self.local_path = os.path.join(config.get_property('export_dir'), self.filename)
-        self.server_path = os.path.join(host, query_path.format(self.id, archive))
+    def __init__(self, id):
+        self.id = str(id)
+        self.filename = "{}.txt".format(self.id)
+        self.title = None
+        self.creator = [] 
+        self.subject = []
+        self.description = None
+        self.date = None
+        self.type = []
+        self.format = None
+        self.identifier = []
+        self.relation = []
 
+    def parse(self, txt):
+        '''Parse dublin-core keys and values into attributes on the python object'''
+        lines = [line.strip() for line in txt.split('\n') if line.strip() is not '']
+        for n, line in enumerate(lines):
+            # split line on first colon
+            key, value = tuple(line.split(': ', 1))
+            # default to empty list
+            attrib_list = getattr(self, key, [])
+            # update the existing value list and then the attribute
+            attrib_list.append(value)
+            setattr(self, key, attrib_list)
 
-    def check_server(self):
-        response = requests.get(self.server_path)
-        if response.status_code == 200:
-            return response.text
-        else:
-            return None
+    def check_links(self, sitehost):
+        if hasattr(self, 'relation'):
+            for link in self.relation:
+                if link.startswith(sitehost):
+                    self.link_loc = link
+                else:
+                    self.link_ext = link
+        if self.link_ext is not None:
+            requests.get(self.link_ext)
 
-
-    def fetch_metadata(self):
-        if not os.path.isfile(self.local_path):
-            response = self.check_server()
-            if response:
-                with open(self.local_path, 'w') as handle:
-                    handle.write(response)
-            else:
-                return
-        self.parse_metadata_file()
-
-
-    def parse_metadata_file(self):
-    
-        '''Parse a textfile of dublin-core keys and values 
-           into attributes on the python object'''
-           
-        with open(self.local_path, 'r') as inputfile:
-            contents = inputfile.read().split('\n')
-            
-        for n, line in enumerate(contents):
-            # ignore blank lines and lines with only whitespace
-            line = line.strip()
-            if line == '':
-                continue
-            else:
-                # split line on first colon
-                key, value = tuple(line.split(': ', 1))
-                # default to empty list
-                attrib_list = getattr(self, key, [])
-                # update the existing value list and then the attribute
-                attrib_list.append(value)
-                setattr(self, key, attrib_list)
-
+    def fetch_binaries(self):
+        for relation in self.
 
     def display_metadata(self):
         for k, v in self.__dict__.items():
