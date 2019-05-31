1556bcaf20edd41b4922a5913fc822985d93697c
Joshua Westgard <jwestgard@mac.com>
2019-05-31 17:35:11 -0400

    update mapping to make more concise and add replace category

--- e2d/mapping.py
+++ e2d/mapping.py
@@ -2,40 +2,27 @@ fields = [{
     'source':      'title',
     'destination': 'dc.title',
     'required':    True,
-    'unique':      True,
-    'condition':   None,
-    'mapping':     None,
-    'pattern':     None
+    'unique':      True
     },{
     'source':      'creator',
     'destination': 'dc.contributor.author',
     'required':    False,
-    'unique':      False,
-    'condition':   None,
-    'mapping':     None,
-    'pattern':     None
+    'unique':      False
     },{
     'source':      'contributor',
     'destination': 'dc.contributor.author',
     'required':    False,
-    'unique':      False,
-    'condition':   None,
-    'mapping':     None,
-    'pattern':     None
+    'unique':      False
     },{
     'source':      'publisher',
     'destination': 'dc.contributor.publisher',
     'required':    False,
-    'unique':      True,
-    'condition':   None,
-    'mapping':     None,
-    'pattern':     None
+    'unique':      True
      },{
     'source':      'type',
     'destination': 'dc.type',
     'required':    False,
     'unique':      False,
-    'condition':   None,
     'mapping': {
         'Article':                              'Article',
         'Book':                                 'Book',
@@ -50,64 +37,75 @@ fields = [{
         'Other':                                'Other',
         'PeerReviewed':                         None,
         'NonPeerReviewed':                      None
-        },
-    'pattern':     None
+        }
     },{
     'source':      'identifier',
     'destination': 'dc.identifier.citation',
     'required':    False,
     'unique':      False,
-    'condition':   lambda x: not x.startswith('http://health-equity.lib.umd.edu'),
-    'mapping':     None,
-    'pattern':     None
+    'condition':   lambda x: not x.startswith(
+                        'http://health-equity.lib.umd.edu'
+                        )
+    'replace':      r'((ISBN|ISSN) *[\dX-]+)\s'
     },{
     'source':      'identifier',
     'destination': 'binaries',
     'required':    False,
     'unique':      False,
-    'condition':   lambda x: x.startswith('http://health-equity.lib.umd.edu'),
-    'mapping':     None,
-    'pattern':     None
+    'condition':   lambda x: x.startswith(
+                        'http://health-equity.lib.umd.edu'
+                        )
     },{
     'source':      'relation',
     'destination': 'dc.identifier.other',
     'required':    True,
     'unique':      False,
-    'condition':   lambda x: x.startswith('http://health-equity.lib.umd.edu'),
-    'mapping':     None,
-    'pattern':     r'http://health-equity\.lib\.umd\.edu/(\d+)'
+    'condition':   lambda x: x.startswith(
+                        'http://health-equity.lib.umd.edu'
+                        ),
+    'match':       r'http://health-equity\.lib\.umd\.edu/(\d+)'
     },{
     'source':      'relation',
     'destination': 'dc.description.uri',
     'required':    False,
     'unique':      False,
-    'condition':   lambda x: not x.startswith('http://health-equity.lib.umd.edu'),
-    'mapping':     None,
-    'pattern':     None
+    'condition':   lambda x: not x.startswith(
+                        'http://health-equity.lib.umd.edu'
+                        )
     },{
     'source':      'date',
     'destination': 'dc.date.issued',
     'required':    True,
     'unique':      True,
-    'condition':   None,
-    'mapping':     None,
-    'pattern':     r'(\d{4})'
+    'match':       r'(\d{4})'
     },{
     'source':      'identifier',
     'destination': 'dc.identifier.isbn',
     'required':    False,
     'unique':      False,
-    'condition':   lambda x: not x.startswith('http://health-equity.lib.umd.edu'),
-    'mapping':     None,
-    'pattern':     r'ISBN\s+([\S]+)'
+    'condition':   lambda x: not x.startswith(
+                        'http://health-equity.lib.umd.edu'
+                        ),
+    'match':       r'ISBN\s+([\S]+)'
     },{
     'source':      'identifier',
     'destination': 'dc.identifier.issn',
     'required':    False,
     'unique':      False,
-    'condition':   lambda x: not x.startswith('http://health-equity.lib.umd.edu'),
-    'mapping':     None,
-    'pattern':     'ISSN\s+([\S]+)'
+    'condition':   lambda x: not x.startswith(
+                        'http://health-equity.lib.umd.edu'
+                        ),
+    'match':       r'ISSN\s+([\S]+)'
+    },{
+    'source':      'subject',
+    'destination': 'dc.subject',
+    'required':    False,
+    'unique':      False
+    },{
+    'source':      'description',
+    'destination': 'dc.description.abstract',
+    'required':    False,
+    'unique':      False
     }
 ]
 
